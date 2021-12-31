ad1d71a33e2eb0eeb2e413e860d787b518a9412e
Muhie <85196209+Muhie@users.noreply.github.com>
2021-12-31 23:58:07 +0000

    Update v1.1

--- main.dart
+++ main.dart
@@ -1,24 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+void main() => runApp(const MyApp());
 
-void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-    
 
-  ));
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Theme data',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        /* light theme settings */
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        /* dark theme settings */
+      ),
+      themeMode: ThemeMode.system, 
+      /* ThemeMode.system to follow system theme, 
+         ThemeMode.light for light theme, 
+         ThemeMode.dark for dark theme
+      */
+      debugShowCheckedModeBanner: false,
+      home: const Page1(),
+      
+    );
+  }
+}
+class Page1 extends StatelessWidget {
+  const Page1({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Muhies Mobile app!')), 
+      body: Center(child: ElevatedButton(
+        onPressed: (){
+          Navigator.of(context)
+          .push(
+            MaterialPageRoute(
+              builder: (context) => const Page2()
+            )
+          );
+        },
+        child: const Text("Click Here to open the menu"),
+      )),
+    );
+  }
 }
 
-class HomePage extends StatefulWidget {
-  @override 
-  _HomePageState createState() => _HomePageState();
+class Page2 extends StatefulWidget {
+  const Page2({ Key? key }) : super(key: key);
+
+  @override
+  _Page2State createState() => _Page2State();
 }
 
-class _HomePageState extends State<HomePage> {
-
-
-
-  String data = '';
+class _Page2State extends State<Page2> {
+    String data = '';
   fetchFileData() async{
     String responseText;
     responseText = await rootBundle.loadString('textFiles/textDemo.txt');
@@ -31,16 +71,17 @@ class _HomePageState extends State<HomePage> {
     fetchFileData();
     super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Muhies amazing work'),
-      ),
-      body: (
-        Text(data)
-        ),   
+      appBar: AppBar(title: const Text('Muhies app page 2')),
+      body: Center(child: ElevatedButton(
+        onPressed: (){
+          Navigator.of(context)
+          .pop();
+        },
+        child: Text(data),
+      )),
     );
   }
 }
