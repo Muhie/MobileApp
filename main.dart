ffd00ee9b77e8ec364d18b76470409fe626cbd2f
Muhie <85196209+Muhie@users.noreply.github.com>
2022-01-05 19:34:12 +0000

    V2.2 Final

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 void main() => runApp(const MyApp());
@@ -83,21 +85,25 @@ class _Page2State extends State<Page2> {
       body: Center(child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-        const Text("Welcome To My Absolutely Crazy App!"),
-          ElevatedButton(
+        const Padding(padding: EdgeInsets.all(20),
+          child: Text("Welcome To My Absolutely Crazy App!"),
+        ),
+        Padding(padding: EdgeInsets.all(20),
+          child: ElevatedButton(
             style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
             onPressed: (){
-            Navigator.of(context)
-            .push(
-              MaterialPageRoute(
-                builder: (context) => const InfoPage1()
-              )
+              Navigator.of(context)
+              .push(
+                MaterialPageRoute(
+                  builder: (context) => const InfoPage1()
+                  )
             );
             },
           child: const Text("What are System Development Lifecycles?")
           // dont forget to add attributes here later 
-          ,),
-          ElevatedButton(
+          ,)),
+          Padding(padding: EdgeInsets.all(20),
+          child: ElevatedButton(
             style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
             onPressed: (){
             Navigator.of(context)
@@ -107,10 +113,11 @@ class _Page2State extends State<Page2> {
               )
             );
             },
-          child: const Text("Agile Model")
+          child: const Text("          What exactly is the Agile Model?          ")
           // dont forget to add attributes here later
-          ,),
-          ElevatedButton(
+          ,)),
+          Padding(padding: EdgeInsets.all(20),
+          child: ElevatedButton(
             style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
             onPressed: (){
             Navigator.of(context)
@@ -120,10 +127,11 @@ class _Page2State extends State<Page2> {
               )
             );
             },
-          child: const Text("Waterfall Model")
+          child: const Text("    What exactly is the Waterfall Model?    ")
           // dont forget to add attributes here later
-          ,),
-          ElevatedButton(
+          ,)),
+          Padding(padding: EdgeInsets.all(20),
+          child: ElevatedButton(
             style: ElevatedButton.styleFrom(shape: const StadiumBorder()),
             onPressed: (){
             Navigator.of(context)
@@ -133,9 +141,9 @@ class _Page2State extends State<Page2> {
               )
             );
             },
-          child: const Text("Spiral Model")
+          child: const Text("        What exactly is the Spiral Model?        ")
           // dont forget to add attributes here later
-          ,),                      
+          ,)),                      
         ],
       )),
     );
@@ -176,9 +184,7 @@ class _InfoPage1State extends State<InfoPage1> {
       children: [
          Text(data,style: TextStyle(fontSize: 20.0*textscale)),
           const Image(
-        image: NetworkImage('https://images.techopedia.com/definition/term-image/22193/software-development-life-cycle-sdlc')),
-          const Image(
-        image: NetworkImage('https://images.techopedia.com/definition/term-image/22193/software-development-life-cycle-sdlc')),
+        image: AssetImage('assets/SDLC-Models.jpg')),
         Text(data2,style: TextStyle(fontSize: 20.0*textscale)),
       ]
       
@@ -200,9 +206,12 @@ class _InfoPage2State extends State<InfoPage2> {
   String data2 = '';
   fetchFileData() async{
     String responseText;
+    String responseText2;
     responseText = await rootBundle.loadString('textFiles/AgileMethod.txt');
+    responseText2 = await rootBundle.loadString('textFiles/AgileMethood2.txt');
     setState(() {
       data = responseText;
+      data2 = responseText2;
   });
   }
   @override
@@ -220,9 +229,8 @@ class _InfoPage2State extends State<InfoPage2> {
       children: [
          Text(data,style: TextStyle(fontSize: 20.0*textscale)),
           const Image(
-        image: NetworkImage('https://images.techopedia.com/definition/term-image/22193/software-development-life-cycle-sdlc')),
-          const Image(
-        image: NetworkImage('https://images.techopedia.com/definition/term-image/22193/software-development-life-cycle-sdlc'))
+        image: AssetImage('assets/Adg.jpg')),
+        Text(data2,style: TextStyle(fontSize: 20.0*textscale)),
       ]
       
     )
@@ -265,7 +273,7 @@ class _InfoPage3State extends State<InfoPage3> {
       children: [
          Text(data,style: TextStyle(fontSize: 20.0*textscale)),
           const Image(
-        image: NetworkImage('https://images.techopedia.com/definition/term-image/22193/software-development-life-cycle-sdlc')),
+        image: AssetImage('assets/Waterfallimg.jpg')),
           Text(data2,style: TextStyle(fontSize: 20.0*textscale))
       ]
       
@@ -306,11 +314,11 @@ class _InfoPage4State extends State<InfoPage4> {
         return Scaffold(
     appBar: AppBar(
     title: const Text("Muhie's Exceptional Mobile App!")),
-    body: SingleChildScrollView(child: Align(alignment: Alignment.center, child: Column(
+    body: SingleChildScrollView(child: Align(alignment: Alignment.topLeft, child: Column(
       children: [
          Text(data,style: TextStyle(fontSize: 20.0*textscale)),
           const Image(
-        image: NetworkImage('https://images.techopedia.com/definition/term-image/22193/software-development-life-cycle-sdlc')),
+          image: AssetImage('assets/spiralmodelimg.jpg')),
         Text(data2,style: TextStyle(fontSize: 20.0*textscale))
       ]
       
